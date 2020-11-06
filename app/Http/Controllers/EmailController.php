19a6ccc1a8942036e183d23a50504aa9a040e4d8
Diego Mendes <gdmendes55@gmail.com>
2020-11-06 14:43:36 -0400

    envio de email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use GuzzleHttp\Psr7\Request;
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Mail;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -11,24 +11,49 @@ class EmailController extends Model
 {
     //
 
+
     public function sendEmail(Request $request)
     {
 
-        Mail::send(
-            'pages.site.emailcontato',
-            $message = [
-                'nome' => $request->$request->input('nome'),
-                'email' => $request->$request->input('email'),
-                'mensagem' => $request->$request->input('mensagem'),
+        $dados = $request->all();
 
-            ],
-            function ($message) {
-
-                $message->to('diego@bksoft.com.br')->subject('Email de contato');
-            }
+        $chaves = array(
+            'atendimento@cruzesantos.com.br' => 'uK00xNGGiO0AIBUIWRqg',
         );
 
 
-        return response()->json(['message', $message]);
+
+        $token = $chaves[$dados['email_destino']];
+
+        if ($token == $dados['token']) {
+            Mail::send('emailcontato', $dados, function ($message, $dados) {
+                $message->to($dados['email_destino'])->subject('Email enviado pelo site');
+            });
+        } else {
+            return response()->json(['error' => 'Não foi possivel enviar o email verifique as informações']);
+        }
+
+
+        // if ($request->has('chave'))
+        //
+        // Mail::send(
+        //     'emailcontato',
+        //     $messages =  [
+        //         'nome' => $request->input('nome'),
+        //         'telefone' => $request->input('telefone'),
+        //         'email' => $request->input('email'),
+        //         'mensagem' => $request->input('mensagem'),
+
+        //     ],
+        //     function ($message, $dados) {
+
+        //         $message->to($request->input('email_destino'))->subject('Email enviado pelo site');
+        //     }
+        // );
+
+
+
+
+        return response()->json(['Email' => $dados]);
     }
 }
